f07afbf469af0e1b3da62c86256b22ef3f948ee3
Dmitry Mazurkin <druglord@mail.ru>
2020-11-20 14:23:25 +0300

    Выполнил первый урок

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const money = '',
+		 income = '',
+		 addExpenses = '',
+		 deposit = '',
+		 mission = '',
+		 period = '';
+
+
+alert('Hello friends!');
+console.log('И снова привет из Питера');
