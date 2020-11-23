e212d7f39dbc3211a41efffb84c4d50346256a68
Dmitry Mazurkin <druglord@mail.ru>
2020-11-23 18:39:02 +0300

    Выполнил третий урок. Обязательное задание

--- js/script.js
+++ js/script.js
@@ -1,27 +1,54 @@
-const money = 45000,
+'use strict';
+
+const money = +prompt( 'Какой твой доход?', '' ),
 		 income = 'Фриланс',
-		 addExpenses = 'Комуналка, интернет, бензин',
-		 deposit = true,
-		 mission = 150000,
+		 addExpenses = prompt( 'Перечислите возможные расходы за рассчитываемый период через запятую', '' ),
+		 deposit = confirm( 'Есть ли у вас депозит в банке?' ),
+		 mission = 100000,
 		 period = 6;
+
+let expenses1 = prompt('Введите обязательную статью расходов?');
+let amount1;
+	while ( isNaN( amount1 = +prompt( 'Во сколько ' + expenses1 + ' обойдется?' ) ) ); // проверяем на NaN
+let expenses2 = prompt( 'Введите обязательную статью расходов?' );
+let amount2;
+	while ( isNaN( amount2 = +prompt( 'Во сколько ' + expenses2 + ' обойдется?' ) ) ); // проверяем на NaN
+
+let budgetMonth = money - ( amount1 + amount2 );
+
+let target = Math.ceil( mission / budgetMonth );
 
 let budgetDay = money / 30;
 
+
 	// Вывод типа данных
-console.log('Тип данных переменной "money": ', typeof money);
-console.log('Тип данных переменной "income": ', typeof income);
-console.log('Тип данных переменной "deposit": ', typeof deposit);
+console.log( 'Тип данных переменной "money": ', typeof money );
+console.log( 'Тип данных переменной "income": ', typeof income );
+console.log( 'Тип данных переменной "deposit": ', typeof deposit );
 
 	//Вывод длины строки
-console.log('Длинная строки переменной "addExpenses": ', addExpenses.length);
+console.log( 'Длинная строки переменной "addExpenses": ', addExpenses.length );
 
 	//Вывод периода и цели
-console.log('Период равен ' + period + ' ' + 'месяцев');
-console.log('Цель заработать ' + mission + ' рублей');
+console.log( 'Период равен ' + period + ' ' + 'месяцев' );
+console.log( 'Цель заработать ' + mission + ' рублей' );
 
 	// Вывод строки в нижнем регистре
-console.log('Нижний регистр: ', addExpenses.toLowerCase());
-console.log('Массив: ', addExpenses.split(', '));
+console.log( 'Нижний регистр: ', addExpenses.toLowerCase( ) );
+console.log( 'Массив: ', addExpenses.split( ', ' ) );
+
+console.log( 'Месячный бюджет: ', budgetMonth );
+
+console.log( 'Цель будет достигнута за ' + target + (target==1?' месяц.':( (target>1&&target<5)?' месяца.':' месяцев.')));
 
 	// Вывод переменной budgetDay
-console.log('Дневной бюджет: ', budgetDay);
+console.log( 'Дневной бюджет: ', budgetDay );
+
+	// Условия вывода переменной budgetDay
+if (budgetDay >= 1200) {
+	console.log( 'У вас высокий уровень дохода' );
+} else if (budgetDay <= 600) {
+	console.log( 'У вас средний уровень дохода' );
+} else {
+	console.log( 'К сожалению у вас уровень дохода ниже среднего' );
+}
