d2fdbda0cb6c08a93fd831dbb26de4ac6b973cc0
Dmitry Mazurkin <druglord@mail.ru>
2020-12-25 16:45:02 +0300

    Урок 19. Обязательное задание

--- scripts/script.js
+++ scripts/script.js
@@ -38,7 +38,74 @@ window.addEventListener('DOMContentLoaded', function() {
 
 		updateClock();
 		let timeInterval = setInterval(updateClock, 1000);
-	}
+	};
 
 	countTimer('31 december 2020');
+
+	// Menu
+	const toggleMenu = () => {
+		const btnMenu = document.querySelector('.menu'),
+			menu = document.querySelector('menu'),
+			closeBtn = document.querySelector('.close-btn'),
+			menuItem = menu.querySelectorAll('ul>li');
+
+		const handlerMenu = () => {
+			menu.classList.toggle('active-menu');
+			// if (!menu.style.transform || menu.style.transform === `translate(-100%)`) {
+			// 	menu.style.transform = `translate(0)`;
+			// } else {
+			// 	menu.style.transform = `translate(-100%)`;
+			// }
+		};
+
+		btnMenu.addEventListener('click', handlerMenu);
+		closeBtn.addEventListener('click', handlerMenu);
+		menuItem.forEach(elem => elem.addEventListener('click', handlerMenu));
+	};
+
+	toggleMenu();
+
+	// Popup
+	const togglePopup = () => {
+		const popup = document.querySelector('.popup'),
+		popupBtn = document.querySelectorAll('.popup-btn'),
+		popupClose = document.querySelector('.popup-close'),
+		popupContent = document.querySelector('.popup-content'),
+		popupData = {
+			count: 460,
+			speed: 15,
+			start: 460,
+			end: -50
+		};
+
+		const showPopup = () => {
+
+			popupData.start > popupData.end ? popupData.count -= popupData.speed : popupData.count += popupData.speed;
+			popupContent.style.transform = `translateX(${popupData.count}px)`;
+
+			if (popupData.start > popupData.end ?
+				popupData.count > popupData.end :
+				popupData.count < popupData.end) {
+				requestAnimationFrame(showPopup);
+			}
+		};	
+
+		popupBtn.forEach((elem) => {
+			elem.addEventListener('click', () => {
+				popup.style.display = 'block';
+				//requestAnimationFrame(showPopup);
+				if (screen.width > 768) {
+					popupData.count = popupData.start;
+					requestAnimationFrame(showPopup);
+				}
+			})
+		});
+
+		popupClose.addEventListener('click', () => {
+			popup.style.display = 'none';
+			popupData.count = popupData.start;
+		});
+	}
+
+	togglePopup();
 });
